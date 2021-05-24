71fae0fdb13642449fae05169dd8fb9678a36dab
Amar Oklopcic <amar_oklopcic@live.com>
2021-05-23 23:53:08 -0500

    Fixed relative imports in __init__.py

--- __init__.py
+++ __init__.py
@@ -3,10 +3,12 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
+apps_directory = os.path.join(os.path.dirname(__file__), "apps")
+
 # automatically pull in blueprints from ./apps directory
-for app_directory in os.listdir("apps"):
-    if "blueprint.py" in os.listdir(f"apps/{app_directory}"):
-        module = importlib.import_module(f"apps.{app_directory}.blueprint")
+for app_directory in os.listdir(apps_directory):
+    if "blueprint.py" in os.listdir(f"{apps_directory}/{app_directory}"):
+        module = importlib.import_module(f".apps.{app_directory}.blueprint", __name__)
         app.register_blueprint(module.blueprint, url_prefix=f"/{app_directory}")
 
 @app.route("/")
